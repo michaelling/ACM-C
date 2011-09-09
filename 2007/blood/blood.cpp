25f963debd6fadf7604bb1c12ca080dfee4ac866
Michael <me@michael-ling.com>
2011-09-08 21:17:24 -0400

    added problem a+ : blood.cpp

---
+++ 2007/blood/blood.cpp
@@ -0,0 +1,29 @@
+/*
+
+	filename:		blood.cpp	
+	author:			michael ling
+
+	description:	ACM ICPC Problem A+
+
+*/
+
+#include <iostream>
+#include <string>
+
+int main(int argc, *char[] argv)
+{
+	char aboallele1, aboallele2;			
+	string abobloodtype;
+	
+/*	
+	A + A = A
+	A + B = AB
+	A + O = A
+	B + B = B
+	B + O = B
+	O + O = O
+
+*/
+			
+	return 0;
+}
